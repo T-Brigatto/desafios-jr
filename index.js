4aa1b075dc9b2240b185a824e250d4e20da7570b
Thiago Brigatto <tbrigatto.p@gmail.com>
2022-04-07 13:04:55 -0300

    Alterações que evitam sobra no saque (como em números ímpares)

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 const cedulas = [2, 5, 10, 20, 50];
+const j = 0;
 
-// processarSaque() retorna quantas cédulas da nota atual serão retiradas e o valor final retirado
 function realizarSaque(valorSaque) {
+
     let saldo = Number(valorSaque);
     let relatorio = [];
-    console.log(`***************************************`);
+    console.log(`***************************************\n`);
     console.log(`SALDO INICIAL: ${saldo}\n`)
-    if (saldo % 2 == 0) {
+
+    if ((saldo != 3) && (saldo != 1)) {
+
         for (let i = cedulas.length - 1; i >= 0; i--) {
+
             let cedulaSelecionada = Number(cedulas[i]);
             let qtdCedulas = contarCedulas(saldo, cedulaSelecionada);
             let valorRetirado = somarCedulasRetiradas(qtdCedulas, cedulaSelecionada);
 
-            // Adiciona as cédulas sacadas ao relatório
-            // Usando o spread operator para "destrinchar" a array e adicionando os valores no final do relatório
+            // Se a cédula for selecionada, as cédulas são adicionadas ao array relatorio
             qtdCedulas > 0 ? relatorio.unshift(...atualizarRelatorio(qtdCedulas, cedulaSelecionada)) : null;
 
-            mostrarSaque(cedulaSelecionada, qtdCedulas, valorRetirado);
+            // Retira-se do valor do saque a soma das cédulas selecionadas
             saldo -= valorRetirado;
+
+            // Verifica se há sobra no saldo, se sim o looping é feito novamente com novos valores
+            if (saldo === 1) {
+                let temp = relatorio.findIndex((index) => {
+                    return index >= 5;
+                });
+                saldo = saldo + relatorio[temp] - 6;
+                relatorio.splice(temp, 1, 2, 2, 2);
+                i = cedulas.length;
+            }
+
+            // Verifica se o saldo está zerado, se sim o looping é quebrado para evitar cálculos desnecessários
+            if (saldo === 0) {
+                break;
+            }
         }
     }
     finalizarSaque(saldo, relatorio);
-}
-
-// mostrarSaque() retorna uma string com saldo atual e final, além das operações realizadas
-// A função não mostra na tela cédulas não utilizadas
-function mostrarSaque(cedulaSelecionada, qtdCedulas, valorRetirado) {
-    if (qtdCedulas != 0) {
-        console.log(`- ${valorRetirado}\n${qtdCedulas} cédula(s) de R$ ${cedulaSelecionada}\n`)
-    }
 }
 
 // Conta a quantidade de notas que "cabem" dentro do valor requisitado
@@ -41,7 +51,7 @@ function somarCedulasRetiradas(quantidade, cedulaSelecionada) {
     return quantidade * cedulaSelecionada;
 }
 
-// Retorna uma string com as cédulas que foram adicionadas ao saque
+// Retorna as cédulas que foram adicionadas ao saque
 function atualizarRelatorio(qtdCedulas, cedulaSelecionada) {
     let relatorio = [];
     for (i = 0; i < qtdCedulas; i++) {
@@ -52,7 +62,7 @@ function atualizarRelatorio(qtdCedulas, cedulaSelecionada) {
 
 function finalizarSaque(saldo, relatorio) {
     saldo === 0 ? console.log(`*** ÊXITO! ***\n\SALDO FINAL | R$ ${saldo}\n`) : console.log(`*** FALHA! ***\nApenas cédulas:`, cedulas, `\n`)
-    // console.log(`NOTAS RETIRADAS:`, relatorio.length, `\n`, relatorio, `\n`);
+    console.log(`NOTAS RETIRADAS:`, relatorio.length, `\n`, relatorio, `\n`);
 }
 
 console.log(`\n`);
